25a307a43bbfadbd174e51634b65e91da25e50b4
wolfofRome <prideofguardian0607@gmail.com>
2020-11-16 20:36:37 +0000

    Add Floor01 & Floor02 scenes

---
+++ Assets/Scripts/ToFloor01.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToFloor01 : MonoBehaviour
+{
+    [SerializeField] private bool isEntered;
+
+    private void OnTiggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            isEntered = true;
+            SceneManager.LoadScene("Floor01");
+        }
+    }
+}
