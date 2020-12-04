47f03a293b06b607c427b993184964600ae56c5a
wolfofRome <prideofguardian0607@gmail.com>
2020-12-04 16:03:09 +0800

    Add Scrips about menu and scenes switch
    Note that the total menu script is ‘Menu1.cs’ !

---
+++ Assets/Scripts/EnterNextFloor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class EnterNextFloor : MonoBehaviour
+{
+    public GameObject GameSaveManager;
+    public GameObject loadScreen;
+    public Slider slider;
+    public Text text;
+    private GameSaveManager manager;
+    // Scene switching
+    IEnumerator LoadLevel()
+    {
+        loadScreen.SetActive(true);
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        operation.allowSceneActivation = false;
+        
+        while(!operation.isDone)
+        {
+            slider.value = operation.progress;
+            text.text = operation.progress * 100 + "%";
+
+            if(operation.progress >= 0.9f)
+            {
+                slider.value = 1;
+
+                text.text = "Press Space to continue";
+
+                if(Input.GetKeyDown(KeyCode.Space))
+                {
+                    operation.allowSceneActivation = true;
+                }
+            }
+            yield return null;
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            manager = GameSaveManager.GetComponent<GameSaveManager>();
+            manager.SaveGame();
+            StartCoroutine(LoadLevel());
+            manager.LoadGame();
+        }
+    }
+}
