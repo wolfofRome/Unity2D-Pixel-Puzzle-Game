9408685e84b00e5db70b469d4c8ea66dce9badd7
wolfofRome <prideofguardian0607@gmail.com>
2020-12-07 10:58:01 +0000

    Update

--- Item/inventory_scripts/Inventory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Inventory", menuName = "Item/New Inventory")]
-public class Inventory : ScriptableObject
-{
-    public List<Item> itemList = new List<Item>();
-}
